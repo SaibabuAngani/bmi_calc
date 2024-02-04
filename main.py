b1bd96cc5bad84e5137182e84bb60b5e97b2a5fe
SaibabuAngani <146207336+SaibabuAngani@users.noreply.github.com>
2024-02-04 13:50:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-height=float(input('enter height in inches'))
-weight=float(input('enter weightnin lbs'))
-def bmi(height,weight):
-     bmi=weight/(height**2)*703
-     if(bmi<16):
-         return 'severly underweight',bmi
-     elif(bmi>=16 and bmi<18.5):
-         return 'underweight',bmi
-     elif(bmi>=18.5 and bmi<25):
-         return 'healthy',bmi
-     elif(bmi>=25 and bmi<30):
-         return 'overweight',bmi
-     elif(bmi>=30):
-         return 'obese',bmi
-quote,bmi=bmi(height,weight)
-print('your bmi is:{} and you are:{}'.format(bmi,quote))
-         
+from bmi_lib import bmi_calc, bmi_catogorize, solti_input
+
+def babu():
+    print("Welcome to the Saibabu BMI Calculator!")
+
+    weight, height = solti_input()
+
+    bmi = bmi_calc(weight, height)
+    category = bmi_catogorize(bmi)
+
+    print("\nBMI Result:")
+    print(f"BMI: {bmi:.2f}")
+    print(f"Category: {category}")
+
+if __name__ == "__main__":
+    babu()
